18808ca2ba0e05920806745eb65ac9d737a9fc87
Greshma Shaji <43352861+ceccs17d24@users.noreply.github.com>
2018-11-29 16:21:40 +0530

    Update 20.Mul_Stack_U_Array.c

--- 20.Mul_Stack_U_Array.c
+++ 20.Mul_Stack_U_Array.c
@@ -128,3 +128,82 @@ int main()
   }
   return 0;
 }
+
+    OUTPUT:
+
+
+    We can push a total of 50 values
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :1
+
+    enter data :4
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :1
+
+    enter data :4
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :2
+
+    enter data :5
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :2
+
+    enter data :6
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :5
+    stack 1:
+    4 4
+    stack 2:
+    5 6
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :3
+    0 is being popped from Stack 1
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :5
+    stack 1:
+    4
+    stack 2:
+    5 6
+    1. push in stack 1
+    2. push in stack 2
+    3. pop from stack 1
+    4. pop from stack 2
+    5.display
+    6. exit
+    enter choice :6
+              
