61be30bc908f6f8de2f2ac30999d727954a35f44
Greshma Shaji <43352861+ceccs17d24@users.noreply.github.com>
2018-11-29 15:32:49 +0530

    Update 4.Bubble_sort.c

--- 4.Bubble_sort.c
+++ 4.Bubble_sort.c
@@ -2,15 +2,15 @@
           //ROLL NO:24
 
 #include<stdio.h>
-main()
+int main()
 {
 	int a[20],i,j,temp;
 	printf("Enter size:");
 	scanf("%d",&n);
 	printf("Enter elements:");
-	for(i=0;i<n;i++)
+	for(i=1;i<n;i++)
 	{
-		for(j=0;j<n-i-1;j++)
+		for(j=0;j<n-i;j++)
 		{
 			if(a[j]>a[j+1])
 			{
@@ -23,5 +23,10 @@ main()
 	printf("The sorted array is:");
 	for(j=0;j<n;j++)
 		printf("%d",a[j]);
+	return 0;
 	
 }
+output:
+  Enter the size of the array: 2 
+enter the elements: 2 3
+array after sorting: 2 3
